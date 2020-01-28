65a525a14ce1c393e2c888d241c43f08e1954018
Frederic Debraine <freddebraine@gmail.com>
2020-01-28 09:08:33 +0100

    chore: minor changes

--- code/bokeh_dashboard.py
+++ code/bokeh_dashboard.py
@@ -2,12 +2,11 @@
 # Model widget (load data, select preprocessing, select model, train button)
 
 import logging
-from bokeh.io import curdoc, output_file, show
-from bokeh.layouts import row, Spacer, column
+from bokeh.io import curdoc
+from bokeh.layouts import row
 from bokeh.models.widgets import Tabs
 from bokeh_widgets.formatter_widget import FormatterWidget
 from bokeh_widgets.controller_widget import ControllerWidget
-from bokeh_widgets.info_widget import InfoWidget
 from bokeh_widgets.visualizer_tab import VisualizerTab
 from bokeh_widgets.model_widget import ModelWidget
 from bokeh_widgets.player_widget import PlayerWidget
