13464ebf6e182d01b4f05da21538e80ff731adb6
Frederic Debraine <freddebraine@gmail.com>
2020-01-28 09:08:07 +0100

    feature: save/load model

--- code/feature_extraction_functions/models.py
+++ code/feature_extraction_functions/models.py
@@ -1,39 +1,35 @@
 import logging
 import numpy as np
-import os
-import random
+import pickle
+import time
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
 from sklearn.model_selection import StratifiedKFold
 from skopt import BayesSearchCV
-from feature_extraction_functions.fbcsp import CSP, FBCSP
-from feature_extraction_functions.riemann import Riemann
-from feature_extraction_functions.convnets import ShallowConvNet
+from .csp import CSP
+from .fbcsp import FBCSP
+from .riemann import Riemann
+from .convnets import ShallowConvNet
 
 # Reproducibility
 seed_value = 0
-os.environ['PYTHONHASHSEED'] = str(seed_value)
-random.seed(seed_value)
 np.random.seed(seed_value)
 
 
 def get_CSP_model():
     model_name = 'CSP'
     search_space = {'classifier__C': (1e-3, 1e3, 'log-uniform')}
-    model = Pipeline([('feat', CSP()),
-                      ('classifier', SVC(kernel='rbf', gamma='scale', C=10,
-                                         random_state=1))])
+    model = Pipeline(steps=[('feat', CSP()),
+                            ('classifier', SVC())])
+    # ('classifier', SVC(kernel='rbf', gamma='scale', C=10))])
     return model, search_space, model_name
 
 
 def get_FBCSP_model():
     model_name = 'FBCSP'
-    search_space = {  # 'feat__f_order': (1, 5),
-        # 'feat__f_type': ['butter', 'cheby', 'ellip'],
-        'classifier__C': (1e-1, 1e3, 'log-uniform')}
+    search_space = {'classifier__C': (1e-1, 1e3, 'log-uniform')}
     model = Pipeline([('feat', FBCSP(fs=250, f_type='butter', m=2, k=-1)),
-                      ('classifier', SVC(kernel='rbf', gamma='scale', C=10,
-                                         random_state=1))])
+                      ('classifier', SVC(kernel='rbf', gamma='scale', C=10))])
     return model, search_space, model_name
 
 
@@ -70,7 +66,7 @@ def get_model(model_str):
     return model, search_space, model_name
 
 
-def train(model_name, X_train, y_train, n_iters=10):
+def train(model_name, X_train, y_train, n_iters=1):
     # Extract MI phase
     fs = 250
     start = 2.5
@@ -80,19 +76,31 @@ def train(model_name, X_train, y_train, n_iters=10):
 
     # Load model
     model, search_space, model_name = get_model(model_name)
+    start_time = time.time()
 
     # Define split
-    skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=1)
+    skf = StratifiedKFold(n_splits=5, shuffle=True)
     skf = list(skf.split(X_train, y_train))
 
     # Optimize
     logging.info('Starting Bayes optimization')
-    gridSearch = BayesSearchCV(model, search_space, n_iter=n_iters,
-                               scoring='accuracy', cv=skf,
-                               n_jobs=4, verbose=1)
+    gridSearch = BayesSearchCV(model, search_space, cv=skf, n_jobs=1,
+                               refit=True, scoring='accuracy', n_iter=n_iters,)
     gridSearch.fit(X_train, y_train)
 
     scores = [gridSearch.cv_results_[f'split{k}_test_score'][gridSearch.best_index_]
               for k in range(gridSearch.n_splits_)]
+    training_time = time.time() - start_time
 
-    return gridSearch, np.mean(scores), np.std(scores)
+    return gridSearch, np.mean(scores), np.std(scores), training_time
+
+
+def save_model(model, save_path, pkl_filename):
+    model_pkl = open(f'{save_path}/{pkl_filename}', 'wb')
+    pickle.dump(model, model_pkl)
+    model_pkl.close()
+
+
+def load_model(model_path):
+    unpickle = open(model_path, 'rb')
+    return pickle.load(unpickle)
