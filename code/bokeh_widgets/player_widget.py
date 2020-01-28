415aaffe6432a39c207691cdc60716314531cd6c
Frederic Debraine <freddebraine@gmail.com>
2020-01-28 09:07:14 +0100

    feature: getting LSL stream

--- code/bokeh_widgets/player_widget.py
+++ code/bokeh_widgets/player_widget.py
@@ -7,9 +7,12 @@ import subprocess
 import time
 import serial
 import serial.tools.list_ports
+from pylsl import StreamInlet, resolve_streams
 from pyqtgraph.Qt import QtCore
 from bokeh.models.widgets import Div, Select, Button, Toggle
+from bokeh.models import ColumnDataSource
 from bokeh.layouts import widgetbox
+from feature_extraction_functions.models import load_model
 
 
 class PlayerWidget:
@@ -20,15 +23,22 @@ class PlayerWidget:
         self.game_log_reader = None
         self.game_logs_path = '../game/log/raceLog*.txt'
         self.ports = '/dev/ttyACM*'
-        self.log_filename = ''
         self._expected_action = (3, "Rest")
-        self.threadpool = QtCore.QThreadPool()
+        self.thread_log = QtCore.QThreadPool()
 
         # Port event sender
         self.port_sender = None
 
         # Game player
         self.game_player = GamePlayer(self.player_idx)
+
+        # LSL stream reader
+        self.lsl_reader = None
+        self.signal_source = ColumnDataSource(dict(ts=[], data=[]))
+        self.thread_lsl = QtCore.QThreadPool()
+
+        # Model
+        self.model = None
 
     @property
     def autoplay(self):
@@ -69,56 +79,63 @@ class PlayerWidget:
         elif sys.platform == 'win32':
             return [p.device for p in serial.tools.list_ports.comports()]
 
+    @property
+    def get_lsl_data(self):
+        assert self.lsl_reader is not None, 'Please connect to a LSL stream'
+        return self.signal_source.data['ts'], self.signal_source.data['data']
+
     def on_launch_game(self):
         logging.info('Lauching Cybathlon game')
         game = subprocess.Popen('../game/brainDriver', shell=False)
         assert game is not None, 'Can\'t launch game !'
+
+        # Wait for logfile to be created
         time.sleep(5)
-        self.select_logfile.options = [''] + glob.glob(self.game_logs_path)
+        log_filename = glob.glob(self.game_logs_path)[-1]
 
-    def on_select_logfile(self, attr, old, new):
-        logging.info(f'Select new log file: {new}')
-        self.log_filename = new
-
-        if self.game_log_reader is None:
-            logging.info('Instanciate log reader')
-            self.game_log_reader = GameLogReader(self, new, self.player_idx)
-            self.threadpool.start(self.game_log_reader)
-        else:
+        # Check if log reader already instanciated
+        if self.game_log_reader is not None:
             logging.info('Delete old log reader')
-            self.threadpool.clear()
+            self.thread_log.clear()
             del self.game_log_reader
-            logging.info('Instanciate new log reader')
-            self.game_log_reader = GameLogReader(self, new, self.player_idx)
-            self.threadpool.start(self.game_log_reader)
+
+        logging.info('Instanciate log reader')
+        self.game_log_reader = GameLogReader(self, log_filename,
+                                             self.player_idx)
+        self.thread_log.start(self.game_log_reader)
+        self.button_launch_game.button_type = 'success'
 
     def on_select_port(self, attr, old, new):
         logging.info(f'Select new port: {new}')
-        if self.port_sender is None:
-            logging.info('Instanciate port sender')
-            logging.info(f'Ports: {self.selected_port}')
-            self.port_sender = CommandSenderPort(new)
-        else:
+
+        if self.port_sender is not None:
             logging.info('Delete old log reader')
             del self.port_sender
-            logging.info('Instanciate port sender')
+
+        logging.info(f'Instanciate port sender {new}')
+        self.port_sender = CommandSenderPort(new)
 
     def on_toggle_autoplay(self, active):
-        assert self.game_log_reader is not None, 'Select log filename first !'
+        # ts, data = self.get_lsl_data
+        # logging.info(f'Timestamps: {ts}')
+        # logging.info(f'Data: {data}')
 
         if active:
-            logging.info('Active autoplay')
+            # TODO: update options
+            assert self.game_log_reader is not None, 'Select log filename first !'
+            logging.info('Activate autoplay')
             self.toggle_autoplay.button_type = 'success'
             self.game_player.sendCommand(self.expected_action[0])
 
         else:
-            logging.info('Inactive autoplay')
+            # TODO: update options
+            assert self.model is not None, 'Load pre-trained model first !'
+            logging.info('Deactivate autoplay')
             self.toggle_autoplay.button_type = 'warning'
+            # TODO: Send model prediction
 
     def on_toggle_send_events(self, active):
-        # Update list of port devices
         self.select_port.options = [''] + self.get_ports
-
         assert self.port_sender is not None, 'Select port first !'
         if active:
             logging.info('Active events sending')
@@ -126,6 +143,26 @@ class PlayerWidget:
         else:
             logging.info('Inactive events sending')
             self.toggle_send_events.button_type = 'warning'
+
+    def on_model_change(self, attr, old, new):
+        logging.info(f'Select new pre-trained model {new}')
+        self.model = load_model(new)
+        self.select_model.options = [''] + glob.glob('./saved_models/*.pkl')
+
+    def on_lsl_connect(self):
+        if self.lsl_reader is not None:
+            logging.info('Delete old lsl stream')
+            self.thread_lsl.clear()
+            del self.lsl_reader
+
+        try:
+            self.lsl_reader = LSLClient(self.signal_source)
+        except Exception:
+            self.lsl_reader = None
+
+        if self.lsl_reader is not None:
+            logging.info('Start LSL thread')
+            self.thread_log.start(self.lsl_reader)
 
     def create_widget(self):
         self.widget_title = Div(text='<b>Player</b>',
@@ -136,11 +173,10 @@ class PlayerWidget:
                                          button_type='primary')
         self.button_launch_game.on_click(self.on_launch_game)
 
-        # Select - Choose game log file to read expected events
-        self.select_logfile = Select(title='Select game log file',
-                                     options=[''])
-        self.select_logfile.options += glob.glob(self.game_logs_path)
-        self.select_logfile.on_change('value', self.on_select_logfile)
+        # Select - Choose pre-trained model
+        self.select_model = Select(title="Pre-trained model")
+        self.select_model.options = [''] + glob.glob('./saved_models/*.pkl')
+        self.select_model.on_change('value', self.on_model_change)
 
         # Toggle - Game is playing in autopilot using logs
         self.toggle_autoplay = Toggle(label='Autoplay',
@@ -157,9 +193,14 @@ class PlayerWidget:
                                          button_type="warning")
         self.toggle_send_events.on_click(self.on_toggle_send_events)
 
+        # Button - Connect to LSL stream TODO: callback
+        self.button_lsl = Button(label='Connect to LSL')
+        self.button_lsl.on_click(self.on_lsl_connect)
+
         # Create layout
         layout = widgetbox([self.widget_title, self.button_launch_game,
-                            self.select_logfile, self.toggle_autoplay,
+                            self.select_model, self.toggle_autoplay,
+                            self.button_lsl,
                             self.select_port, self.toggle_send_events])
         return layout
 
@@ -207,13 +248,13 @@ class GameLogReader(QtCore.QRunnable):
 class GamePlayer:
     def __init__(self, player_idx):
         # Send commands using a separate thread
-        self.threadpool = QtCore.QThreadPool()
+        self.thread_game = QtCore.QThreadPool()
 
     def sendCommand(self, action_idx):
         ''' Send the command to the game after a delay in a separate thread '''
         if action_idx not in [None, 3]:
             command_sender = CommandSenderGame(action_idx)
-            self.threadpool.start(command_sender)
+            self.thread_game.start(command_sender)
 
 
 class CommandSenderGame(QtCore.QRunnable):
@@ -246,3 +287,37 @@ class CommandSenderPort:
     def sendCommand(self, action_idx):
         message = str(action_idx)
         self.serial.write(message.encode())
+
+
+class LSLClient(QtCore.QRunnable):
+    def __init__(self, signal_source):
+        super().__init__()
+
+        logging.info('Looking for LSL stream...')
+        available_streams = resolve_streams(5)
+
+        if len(available_streams) > 0:
+            self.stream_reader = StreamInlet(available_streams[0],
+                                             max_chunklen=1,
+                                             recover=False)
+            id = self.stream_reader.info().session_id()
+            self.fs = int(self.stream_reader.info().nominal_srate())
+            logging.info(f'Stream {id} found at {self.fs} Hz!')
+        else:
+            logging.error('No stream found !')
+            raise Exception
+
+        self.signal_source = signal_source
+
+    @QtCore.pyqtSlot()
+    def run(self):
+        while True:
+            try:
+                data, ts = self.stream_reader.pull_sample()
+            except Exception as e:
+                logging.info(f'{e} - No more data')
+                break
+
+            self.signal_source.stream(dict(ts=[ts],
+                                           data=[data[0]]),
+                                      rollover=10)
