30dc7ecef5e1cb7027a4a428e491f1289baad42e
Frederic Debraine <freddebraine@gmail.com>
2020-01-31 19:43:57 +0100

    chore: restructure  in tabs

--- code/bokeh_dashboard.py
+++ code/bokeh_dashboard.py
@@ -3,34 +3,29 @@
 
 import logging
 from bokeh.io import curdoc
-from bokeh.layouts import row
-from bokeh.models.widgets import Tabs
+from bokeh.models.widgets import Tabs, Panel
 from bokeh_widgets.formatter_widget import FormatterWidget
-from bokeh_widgets.controller_widget import ControllerWidget
-from bokeh_widgets.visualizer_tab import VisualizerTab
+from bokeh_widgets.visualizer_widget import VisualizerWidget
 from bokeh_widgets.model_widget import ModelWidget
 from bokeh_widgets.player_widget import PlayerWidget
+
+document = curdoc()
 
 # Setup logging
 root_logger = logging.getLogger()
 root_logger.setLevel(logging.INFO)
 
 # Widgets and tabs
-formatter = FormatterWidget()
-visu_controller = ControllerWidget()
-trainer = ModelWidget()
-player = PlayerWidget()
-tab1 = VisualizerTab()
-tab2 = VisualizerTab()
-
-visu_controller.attach(tab1)
+formatter = FormatterWidget().create_widget()
+visualizer = VisualizerWidget().create_widget()
+trainer = ModelWidget().create_widget()
+player = PlayerWidget(parent=document).create_widget()
 
 # Layout
-document = curdoc()
-document.add_root(row(formatter.create_widget(),
-                      visu_controller.create_widget(),
-                      trainer.create_widget(),
-                      player.create_widget()))
-tabs = Tabs(tabs=[tab1.create_temporal_tab(),
-                  tab2.create_spectral_tab()])
-document.add_root(row(tabs))
+tab_format = Panel(child=formatter, title='Formatter')
+tab_visualize = Panel(child=visualizer, title='Visualizer')
+tab_train = Panel(child=trainer, title='Trainer')
+tab_play = Panel(child=player, title='Player')
+
+tabs = Tabs(tabs=[tab_format, tab_visualize, tab_train, tab_play])
+document.add_root(tabs)
