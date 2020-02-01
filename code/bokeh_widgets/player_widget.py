aaef7fd0776f4abec1ab29d76c2b792ae481d581
Frederic Debraine <freddebraine@gmail.com>
2020-02-01 21:00:06 +0100

    feature: visualize lsl stream + real-time predictions

--- code/bokeh_widgets/player_widget.py
+++ code/bokeh_widgets/player_widget.py
@@ -17,6 +17,8 @@ from pylsl import StreamInlet, resolve_streams
 from pyqtgraph.Qt import QtCore
 
 from feature_extraction_functions.models import load_model
+from preprocessing_functions.preproc_functions import filtering, rereference
+from preprocessing_functions.preproc_functions import clipping, standardize
 
 
 class PlayerWidget:
@@ -41,14 +43,23 @@ class PlayerWidget:
         self.lsl_reader = None
         self.signal_source = ColumnDataSource(dict(ts=[], data=[]))
         self.thread_lsl = QtCore.QThreadPool()
+        self.callback_lsl_id = None
 
         # Model
         self.model = None
+        self.signal = None
+        self._last_pred = None
+        self.callback_pred_id = None
 
     @property
     def selected_settings(self):
         active = self.checkbox_settings.active
         return [self.checkbox_settings.labels[i] for i in active]
+
+    @property
+    def selected_preproc(self):
+        active = self.checkbox_preproc.active
+        return [self.checkbox_preproc.labels[i] for i in active]
 
     @property
     def autoplay(self):
@@ -88,6 +99,10 @@ class PlayerWidget:
             return glob.glob(self.ports)
         elif sys.platform == 'win32':
             return [p.device for p in serial.tools.list_ports.comports()]
+
+    @property
+    def model_name(self):
+        return self.select_model.value
 
     def on_launch_game(self):
         logging.info('Lauching Cybathlon game')
@@ -132,15 +147,48 @@ class PlayerWidget:
             assert self.game_log_reader is not None, 'Select log filename first !'
             logging.info('Activate autoplay')
             self.game_player.sendCommand(self.expected_action[0])
+
         elif self.should_predict and not self.autoplay:
             assert self.model is not None, 'Load pre-trained model first !'
             logging.info('Activate model prediction')
-            # TODO: Send model prediction
+            self.callback_pred_id = self.parent.add_periodic_callback(self.callback_pred,
+                                                                      250)
+
+    def callback_pred(self):
+        X = np.copy(self.signal)
+        X = np.delete(X, [0, 30], axis=0)
+        logging.info(f'{X.shape} - {self.signal.shape}')
+
+        # Preprocessing
+        if 'Filter' in self.selected_preproc:
+            X = filtering(X, f_low=0.5, f_high=38,
+                          fs=500, f_order=3)
+
+        if 'Standardize' in self.selected_preproc:
+            X = clipping(X, 6)
+            X = standardize(X)
+
+        if 'Rereference' in self.selected_preproc:
+            X = rereference(X)
+
+        # Predict on the most recent 3.5s of signal
+        action_idx = self.model.predict(X[np.newaxis, :, 100:1850])[0]
+        assert action_idx in [0, 1, 2, 3], \
+            'Prediction is not in allowed action space'
+
+        # Send command
+        self.game_player.sendCommand(action_idx)
+        self._last_pred = action_idx
+
+        self.div_info.text = f'<b>Model:</b> {self.model_name} <br>' \
+            f'<b>Groundtruth:</b> {self._expected_action} <br>' \
+            f'<b>Prediction:</b> {self._last_pred} <br>'
 
     def on_model_change(self, attr, old, new):
         logging.info(f'Select new pre-trained model {new}')
         self.model = load_model(new)
         self.select_model.options = [''] + glob.glob('./saved_models/*.pkl')
+        # TODO: Deep learning case
 
     def on_lsl_connect(self):
         if self.lsl_reader is not None:
@@ -150,56 +198,78 @@ class PlayerWidget:
 
         try:
             self.lsl_reader = LSLClient()
+            self.signal = np.zeros((self.lsl_reader.n_channels, 2000))
             if self.lsl_reader is not None:
-                logging.info('Start periodic callback')
-                self.callback_id = self.parent.add_periodic_callback(self.callback_lsl,
-                                                                     100)
+                logging.info('Start periodic callback - LSL')
+                self.select_channel.options = [f'{i} - {ch}' for i, ch
+                                               in enumerate(self.lsl_reader.ch_names)]
+                self.callback_lsl_id = self.parent.add_periodic_callback(self.callback_lsl,
+                                                                         100)
                 self.button_lsl.button_type = 'success'
         except Exception as e:
             logging.info(e)
             self.lsl_reader = None
 
     def callback_lsl(self):
+        ''' Fetch EEG data from LSL stream '''
         data, ts = [], []
         try:
             data, ts = self.lsl_reader.get_data()
+
             if len(data.shape) > 1:
+                # Clean signal and reference TODO: just for visualization purpose
+                data = np.swapaxes(data, 1, 0)
+                data = 1e6 * data
+
+                # Update source
                 self.signal_source.stream(dict(ts=ts,
-                                               data=data[:, 0]),
+                                               data=data[0, :]),
                                           rollover=1000)
+                # Update signal
+                chunk_size = data.shape[-1]
+                self.signal = np.roll(self.signal, -chunk_size, axis=-1)
+                self.signal[:, -chunk_size:] = data
+
         except Exception as e:
             logging.info(f'Ending periodic callback - {e}')
             self.button_lsl.button_type = 'warning'
-            self.parent.remove_periodic_callback(self.callback_id)
+            if self.callback_lsl_id:
+                self.parent.remove_periodic_callback(self.callback_lsl_id)
+            if self.callback_pred_id:
+                self.parent.remove_periodic_callback(self.callback_pred_id)
 
     def create_widget(self):
-        self.widget_title = Div(text='<b>Player</b>',
-                                align='center', style={'color': '#000000'})
-
         # Button - Launch Cybathlon game in new window
         self.button_launch_game = Button(label='Launch Game',
                                          button_type='primary')
         self.button_launch_game.on_click(self.on_launch_game)
+
+        # Button - Connect to LSL stream
+        self.button_lsl = Button(label='Connect to LSL')
+        self.button_lsl.on_click(self.on_lsl_connect)
 
         # Select - Choose pre-trained model
         self.select_model = Select(title="Select pre-trained model")
         self.select_model.options = [''] + glob.glob('./saved_models/*.pkl')
         self.select_model.on_change('value', self.on_model_change)
 
-        # Checkbox - Choose player settings
-        self.checkbox_settings = CheckboxButtonGroup(labels=['Autoplay',
-                                                             'Predict',
-                                                             'Send events'])
-        self.checkbox_settings.on_click(self.on_checkbox_settings_change)
-
         # Select - Choose port to send events to
         self.select_port = Select(title='Select port', options=[''])
         self.select_port.options += self.get_ports
         self.select_port.on_change('value', self.on_select_port)
 
-        # Button - Connect to LSL stream
-        self.button_lsl = Button(label='Connect to LSL')
-        self.button_lsl.on_click(self.on_lsl_connect)
+        # Checkbox - Choose player settings
+        self.div_settings = Div(text='<b>Settings</b>', align='center')
+        self.checkbox_settings = CheckboxButtonGroup(labels=['Autoplay',
+                                                             'Predict',
+                                                             'Send events'])
+        self.checkbox_settings.on_click(self.on_checkbox_settings_change)
+
+        # Checkbox - Choose preprocessing steps
+        self.div_preproc = Div(text='<b>Preprocessing</b>', align='center')
+        self.checkbox_preproc = CheckboxButtonGroup(labels=['Filter',
+                                                            'Standardize',
+                                                            'Rereference'])
 
         # Select - Channel to visualize TODO: get channel names for LSL to get
         self.select_channel = Select(title='Select channel')
@@ -212,12 +282,18 @@ class PlayerWidget:
                                   plot_width=800)
         self.plot_stream.line(x='ts', y='data', source=self.signal_source)
 
+        # Div - Display useful information
+        self.div_info = Div()
+
         # Create layout
-        column1 = column(self.widget_title, self.button_launch_game,
-                         self.button_lsl, self.select_model,
-                         self.select_port, self.checkbox_settings)
+        column1 = column(self.button_launch_game, self.button_lsl,
+                         self.select_model, self.select_port,
+                         self.select_channel,
+                         self.div_settings, self.checkbox_settings,
+                         self.div_preproc, self.checkbox_preproc)
         column2 = column(self.plot_stream)
-        return row(column1, column2)
+        column3 = column(self.div_info)
+        return row(column1, column2, column3)
 
 
 class GameLogReader(QtCore.QRunnable):
@@ -285,6 +361,7 @@ class CommandSenderGame(QtCore.QRunnable):
 
     @QtCore.pyqtSlot()
     def run(self):
+        # TODO: only random delay when autoplay
         time.sleep(np.random.random_sample())
         self.sock.sendto(bytes(self.commands[self.action_idx], "utf-8"),
                          (self.UDP_IP, self.UDP_PORT))
@@ -317,13 +394,24 @@ class LSLClient(QtCore.QRunnable):
                                              recover=False)
             id = self.stream_reader.info().session_id()
             self.fs = int(self.stream_reader.info().nominal_srate())
-            logging.info(f'Stream {id} found at {self.fs} Hz!')
+            self.n_channels = int(self.stream_reader.info().channel_count())
+            logging.info(
+                f'Stream {id} found at {self.fs} Hz with {self.n_channels} channels')
+
+            # Fetching channel names
+            ch = self.stream_reader.info().desc().child('channels').first_child()
+            self.ch_names = []
+            for i in range(self.n_channels):
+                self.ch_names.append(ch.child_value('label'))
+                ch = ch.next_sibling()
+            logging.info(f"Channel names: {self.ch_names}")
         else:
             logging.error('No stream found !')
             raise Exception
 
     def get_data(self):
         try:
+            # Data is of shape (n_timestamps, n_channels)
             data, ts = self.stream_reader.pull_chunk()
         except Exception as e:
             logging.info(f'{e} - No more data')
