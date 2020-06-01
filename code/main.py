6d4735dafda0d946c696fba8d2cf499ac45bb468
Frederic Debraine <freddebraine@gmail.com>
2020-06-01 20:35:46 +0200

    refactor: renaming 'bokeh_dashboard' to 'main'

--- code/main.py
+++ code/main.py
@@ -6,6 +6,7 @@ from bokeh_widgets.formatter_widget import FormatterWidget
 from bokeh_widgets.trainer_widget import TrainerWidget
 from bokeh_widgets.test_widget import TestWidget
 from bokeh_widgets.player_widget import PlayerWidget
+from bokeh_widgets.warmup_widget import WarmUpWidget
 
 document = curdoc()
 
@@ -17,12 +18,14 @@ root_logger.setLevel(logging.INFO)
 formatter = FormatterWidget().create_widget()
 trainer = TrainerWidget().create_widget()
 tester = TestWidget().create_widget()
+warmup = WarmUpWidget(parent=document).create_widget()
 player = PlayerWidget(parent=document).create_widget()
 
 # Layout
 tab_format = Panel(child=formatter, title='Format')
 tab_train = Panel(child=trainer, title='Train')
 tab_test = Panel(child=tester, title='Test')
+tab_warmup = Panel(child=warmup, title='Warmup')
 tab_play = Panel(child=player, title='Play')
-tabs = Tabs(tabs=[tab_format, tab_train, tab_test, tab_play])
+tabs = Tabs(tabs=[tab_format, tab_train, tab_test, tab_warmup, tab_play])
 document.add_root(tabs)
