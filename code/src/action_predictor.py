db45bc90b7175ffe9fd0117bcc06b17530720044
Frederic Debraine <freddebraine@gmail.com>
2020-09-15 00:19:48 +0200

    fix: handling autopilot outputs and avatar control

--- code/src/action_predictor.py
+++ code/src/action_predictor.py
@@ -1,29 +1,58 @@
 import logging
 import copy
 import time
+
 import numpy as np
 from pyqtgraph.Qt import QtCore
 
+from config import main_config, predictor_config, game_config
 from src.pipeline import load_pipeline
 from src.models import predict
+from src.game_player import GamePlayer, CommandSenderPort
 
 
 class ActionPredictor(QtCore.QRunnable):
-    def __init__(self, parent, modelfile, is_convnet, fs=500, predict_every_s=1):
+    def __init__(self, parent, modelfile, is_convnet):
         super(ActionPredictor, self).__init__()
         self.parent = parent
-        self.fs = fs
-        self.ch_to_delete = [0, 30]
-        self.should_reref = True
-        self.should_filter = False
-        self.should_standardize = True
-        self.model = load_pipeline(modelfile)
-        self.is_convnet = is_convnet
-        self.n_crops = 10
-        self.crop_len = 0.5
-        self.predict_every_s = predict_every_s
-        self.action_idx = 0
+        self.fs = main_config['fs']
+        self.pred_decoding = main_config['pred_decoding']
+
+        # Preprocessing
+        self.ch_to_delete = predictor_config['ch_to_delete']
+        self.should_reref = predictor_config['should_reref']
+        self.should_filter = predictor_config['should_filter']
+        self.should_standardize = predictor_config['should_standardize']
+        self.n_crops = predictor_config['n_crops']
+        self.crop_len = predictor_config['crop_len']
+
+        # Prediction
+        self.predict_every_s = predictor_config['predict_every_s']
+        self.fake_delay_min = game_config['fake_delay_min']
+        self.fake_delay_max = game_config['fake_delay_max']
         self.should_predict = True
+        self.action_idx = 0
+
+        # Model
+        self.is_convnet = is_convnet
+        if modelfile == 'AUTOPLAY':
+            self.model = 'AUTOPLAY'
+        else:
+            self.model = load_pipeline(modelfile)
+
+        # Game logs (separate thread - if autoplay)
+        self.game_logs_path = game_config['game_logs_path']
+        self.game_log_reader = None
+        self.thread_log = QtCore.QThreadPool()
+
+        # Game player
+        self.player_idx = game_config['player_idx']
+        self.game_player = GamePlayer(self.player_idx)
+
+    @property
+    def available_logs(self):
+        logs = list(self.game_logs_path.glob(game_config['game_logs_pattern']))
+        return sorted(logs)
 
     def predict(self, X):
         # Removing FP1 & FP2
@@ -32,20 +61,36 @@ class ActionPredictor(QtCore.QRunnable):
         # Selecting last 1s of signal
         X = X[np.newaxis, :, -self.fs:]
 
-        # If no model
         if self.model is None:
             self.action_idx = 0
             logging.warning('Rest action sent by default! '
                             'Please select a model.')
+        elif self.model == 'AUTOPLAY':
+            self.action_idx = self.parent.expected_action[0]
         else:
             self.action_idx = predict(X, self.model, self.is_convnet,
                                       self.n_crops, self.crop_len, self.fs,
                                       self.should_reref, self.should_filter,
                                       self.should_standardize)
-            logging.info(f'Action idx: {self.action_idx}')
+        logging.info(f'Action idx: {self.action_idx}')
+
+        # Send action to avatar (if game is on + not rest command)
+        possible_actions = [k for k in self.pred_decoding.keys()]
+        is_action_command = self.action_idx in possible_actions \
+            and self.pred_decoding[self.action_idx] != 'Rest'
+        if self.parent.game_is_on and is_action_command:
+            logging.info(f'Sending: {self.action_idx}')
+            self.game_player.sendCommand(self.action_idx)
 
     def notify(self):
-        self.parent.current_pred = self.action_idx
+        # AUTOPLAY - Fake delay for more realistic control feel
+        if self.model == 'AUTOPLAY':
+            random_delay = (self.fake_delay_max - self.fake_delay_min) * \
+                np.random.random_sample() + self.fake_delay_min
+            time.sleep(random_delay)
+
+        self.parent.pred_action = (self.action_idx,
+                                   self.pred_decoding[self.action_idx])
 
     @QtCore.pyqtSlot()
     def run(self):
@@ -54,7 +99,7 @@ class ActionPredictor(QtCore.QRunnable):
             countdown = time.time()
             X = copy.deepcopy(self.parent.input_signal)
             self.predict(X)
+            delay_correction = time.time() - countdown
+            time.sleep(self.predict_every_s - delay_correction)
             self.notify()
-            delay = time.time() - countdown
-            time.sleep(self.predict_every_s - delay)
         logging.info('Stop action predictor')
