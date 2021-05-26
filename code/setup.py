3583606ead0fb63538b66fcb213bae8e9ed3621e
Frédéric Debraine <12777340+fdebrain@users.noreply.github.com>
2021-05-26 15:59:22 +0200

    Update setup.py

--- code/setup.py
+++ code/setup.py
@@ -5,6 +5,5 @@ setup(name='master-thesis-cybathlon',
       version='0.1',
       description='',
       author='Frederic Debraine',
-      author_email='freddebraine@gmail.com',
       packages=['src'],
       zip_safe=False)
